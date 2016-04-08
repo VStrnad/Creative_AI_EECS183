bf926f4b744eea4619bf861e23c9d89c5a4854bc
ilansmal <ilansmal@umich.edu>
2016-04-07 20:26:34 -0400

    Added files via upload

--- generate.py
+++ generate.py
@@ -19,96 +19,117 @@ from musicData import *
 
 def trainLyricsModels(lyricsDirectory):
     """
-    Requires: nothing
-    Modifies: nothing
-    Effects:  loads lyrics data from the data/lyrics/<lyricsDirectory> folder
-              using the pre-written DataLoader class, then creates an
-              instance of each of the NGramModel child classes and trains
-              them using the text loaded from the data loader. The list
-              should be in tri-, then bi-, then unigramModel order.
-
-              Returns the list of trained models.
-    """
+        Requires: nothing
+        Modifies: nothing
+        Effects:  loads lyrics data from the data/lyrics/<lyricsDirectory> folder
+        using the pre-written DataLoader class, then creates an
+        instance of each of the NGramModel child classes and trains
+        them using the text loaded from the data loader. The list
+        should be in tri-, then bi-, then unigramModel order.
+        
+        Returns the list of trained models.
+        """
     dataLoader = DataLoader()
     dataLoader.loadLyrics(lyricsDirectory) # lyrics stored in dataLoader.lyrics
     models = [TrigramModel(), BigramModel(), UnigramModel()]
-    TrigramModel.trainModel(dataLoader.lyrics)
-    BigramModel.trainModel(dataLoader.lyrics)
-    UnigramModel.trainModel(dataLoader.lyrics)
+    
+    # add rest of trainLyricsModels implementation here
+    
     return models
 
 def selectNGramModel(models, sentence):
     """
-    Requires: models is a list of NGramModel objects sorted by descending
-              priority: tri-, then bi-, then unigrams.
-    Modifies: nothing
-    Effects:  starting from the beginning of the models list, returns the
-              first possible model that can be used for the current sentence
-              based on the n-grams that the models know. (Remember that you
-              wrote a function that checks if a model can be used to pick a
-              word for a sentence!)
-    """
-    return
+        Requires: models is a list of NGramModel objects sorted by descending
+        priority: tri-, then bi-, then unigrams.
+        Modifies: nothing
+        Effects:  starting from the beginning of the models list, returns the
+        first possible model that can be used for the current sentence
+        based on the n-grams that the models know. (Remember that you
+        wrote a function that checks if a model can be used to pick a
+        word for a sentence!)
+        """
+    
+    if models[0].trainingDataHasNGram:
+        return models[0]
+    elif models[1].trainingDataHasNGram:
+        return models[1]
+    else:
+        return models[2]
 
 def sentenceTooLong(desiredLength, currentLength):
     """
-    Requires: nothing
-    Modifies: nothing
-    Effects:  returns a bool indicating whether or not this sentence should
-              be ended based on its length. This function has been done for
-              you.
-    """
+        Requires: nothing
+        Modifies: nothing
+        Effects:  returns a bool indicating whether or not this sentence should
+        be ended based on its length. This function has been done for
+        you.
+        """
     STDEV = 1
     val = random.gauss(currentLength, STDEV)
     return val > desiredLength
 
 def generateSentence(models, desiredLength):
     """
-    Requires: models is a list of trained NGramModel objects sorted by
-              descending priority: tri-, then bi-, then unigrams.
-              desiredLength is the desired length of the sentence.
-    Modifies: nothing
-    Effects:  returns a list of strings where each string is a word in the
-              generated sentence. The returned list should NOT include
-              any of the special starting or ending symbols.
-
-              For more details about generating a sentence using the
-              NGramModels, see the spec.
-    """
+        Requires: models is a list of trained NGramModel objects sorted by
+        descending priority: tri-, then bi-, then unigrams.
+        desiredLength is the desired length of the sentence.
+        Modifies: nothing
+        Effects:  returns a list of strings where each string is a word in the
+        generated sentence. The returned list should NOT include
+        any of the special starting or ending symbols.
+        
+        For more details about generating a sentence using the
+        NGramModels, see the spec.
+        """
     sentence = ['^::^', '^:::^']
-
+    models = UnigramModel()
+    word = 'hi'
     # add rest of generateSentence implementation here
-
+    while sentenceTooLong == False or models.nexttoken !=  '$::$':
+        modelSelected = selectNGramModel(models, sentence)
+        sentence.append(modelSelected.getNextToken)
+    sentence.remove('^::^')
+    sentence.remove('^:::^')
     return sentence
 
 def printSongLyrics(verseOne, verseTwo, chorus):
     """
-    Requires: verseOne, verseTwo, and chorus are lists of lists of strings
-    Modifies: nothing
-    Effects:  prints the song. This function is done for you.
-    """
-
-    verses = [verseOne, chorus, verseTwo, chorus]
+        Requires: verseOne, verseTwo, and chorus are lists of lists of strings
+        Modifies: nothing
+        Effects:  prints the song. This function is done for you.
+        """
+verses = [verseOne, chorus, verseTwo, chorus]
+print '\n',
+for verse in verses:
+    for line in verse:
+        print (' '.join(line)).capitalize()
     print '\n',
-    for verse in verses:
-        for line in verse:
-            print (' '.join(line)).capitalize()
-        print '\n',
 
 def runLyricsGenerator(models):
     """
-    Requires: models is a list of a trained nGramModel child class objects
-    Modifies: nothing
-    Effects:  generates a verse one, a verse two, and a chorus, then
-              calls printSongLyrics to print the song out.
-    """
+        Requires: models is a list of a trained nGramModel child class objects
+        Modifies: nothing
+        Effects:  generates a verse one, a verse two, and a chorus, then
+        calls printSongLyrics to print the song out.
+        """
     verseOne = []
     verseTwo = []
     chorus = []
 
-    # add rest of runLyricsGenerator implementation here
+# add rest of runLyricsGenerator implementation here
 
-    return
+for i in range(4):
+    verseOne[i]] = generateSentence(models, 5)
+
+for i in range(4):
+    verseTwo[i] = generateSentence(models, 5)
+
+for i in range(4):
+    chorus[i] = generateSentence(models, 5)
+
+printSongLyrics(verseOne,verseTwo,chorus)
+
+return
 
 
 
@@ -119,45 +140,45 @@ def runLyricsGenerator(models):
 
 def trainMusicModels(musicDirectory):
     """
-    Requires: nothing
-    Modifies: nothing
-    Effects:  works exactly as trainLyricsModels from the core, except
-              now the dataLoader calls the DataLoader's loadMusic() function
-              and takes a music directory name instead of an artist name.
-              Returns a list of trained models in order of tri-, then bi-, then
-              unigramModel objects.
-    """
+        Requires: nothing
+        Modifies: nothing
+        Effects:  works exactly as trainLyricsModels from the core, except
+        now the dataLoader calls the DataLoader's loadMusic() function
+        and takes a music directory name instead of an artist name.
+        Returns a list of trained models in order of tri-, then bi-, then
+        unigramModel objects.
+        """
     dataLoader = DataLoader()
     dataLoader.loadMusic(musicDirectory) # music stored in dataLoader.songs
     models = [TrigramModel(), BigramModel(), UnigramModel()]
-
+    
     # add rest of trainMusicModels implementation here
-
+    
     return models
 
 def generateMusicalSentence(models, desiredLength, possiblePitches):
     """
-    Requires: possiblePitches is a list of pitches for a musical key
-    Modifies: nothing
-    Effects:  works exactly like generateSentence from the core, except
-              now we call the NGramModel child class' getNextNote()
-              function instead of getNextToken(). Everything else
-              should be exactly the same as the core.
-    """
+        Requires: possiblePitches is a list of pitches for a musical key
+        Modifies: nothing
+        Effects:  works exactly like generateSentence from the core, except
+        now we call the NGramModel child class' getNextNote()
+        function instead of getNextToken(). Everything else
+        should be exactly the same as the core.
+        """
     sentence = ['^::^', '^:::^']
-
+    
     # add rest of generateMusicalSentence implementation here
-
+    
     return sentence
 
 def runMusicGenerator(models, songName):
     """
-    Requires: models is a list of trained models
-    Modifies: nothing
-    Effects:  runs the music generator as following the details in the spec.
-
-              Note: For the core, this should print "Under construction".
-    """
+        Requires: models is a list of trained models
+        Modifies: nothing
+        Effects:  runs the music generator as following the details in the spec.
+        
+        Note: For the core, this should print "Under construction".
+        """
     print 'Under construction'
 
 
@@ -167,81 +188,85 @@ def runMusicGenerator(models, songName):
 
 def getUserInput(teamName, lyricsSource, musicSource):
     """
-    Requires: nothing
-    Modifies: nothing
-    Effects:  prints a welcome menu for the music generator and prints the
-              options for the generator. Loops while the user does not input
-              a valid option. When the user selects 1, 2, or 3, returns
-              that choice.
-
-              Note: this function is for the reach only. It is done for you.
-    """
+        Requires: nothing
+        Modifies: nothing
+        Effects:  prints a welcome menu for the music generator and prints the
+        options for the generator. Loops while the user does not input
+        a valid option. When the user selects 1, 2, or 3, returns
+        that choice.
+        
+        Note: this function is for the reach only. It is done for you.
+        """
     print 'Welcome to the', teamName, 'music generator!\n'
     prompt = 'Here are the menu options:\n' + \
-             '(1) Generate song lyrics by ' + lyricsSource + '\n' \
-             '(2) Generate a song using data from ' + musicSource + '\n' \
-             '(3) Quit the music generator\n'
+        '(1) Generate song lyrics by ' + lyricsSource + '\n' \
+            '(2) Generate a song using data from ' + musicSource + '\n' \
+                '(3) Quit the music generator\n'
+prompt = 'prompt'
 
-    userInput = -1
-    while userInput < 1 or userInput > 3:
-        print prompt
-        userInput = raw_input('Please enter a choice between 1 and 3: ')
-        try:
-            userInput = int(userInput)
-        except ValueError:
-            userInput = -1
+userInput = -1
+while userInput < 1 or userInput > 3:
+    print prompt
+    userInput = raw_input('Please enter a choice between 1 and 3: ')
+    try:
+        userInput = int(userInput)
+    except ValueError:
+        userInput = -1
 
-    return userInput
-
-def main():
-    """
-    Requires: nothing
-    Modifies: nothing
-    Effects:  this is your main function, which is done for you. It runs the
-              entire generator program for both the reach and the core.
-              It begins by loading the lyrics and music data, then asks the
-              user to input a choice to generate either lyrics or music.
-
-              Note that for the core, only choice 1 (the lyrics generating
-              choice) needs to be completed; if the user inputs 2, you
-              can just have the runMusicGenerator function print "Under
-              construction."
-
-              Also note that you can change the values of the first five
-              variables based on your team's name, artist name, etc.
-    """
-    teamName = 'Team Name'
-    lyricsSource = 'The Beatles'
-    musicSource = 'Nintendo Gamecube'
-    lyricsDirectory = 'the_beatles'
-    musicDirectory = 'gamecube'
-
-    print 'Starting program and loading data...'
-    lyricsModels = trainLyricsModels(lyricsDirectory)
-    musicModels = trainMusicModels(musicDirectory)
-    print 'Data successfully loaded\n'
-
-    userInput = getUserInput(teamName, lyricsSource, musicSource)
-
-    while userInput != 3:
-        print '\n',
-        if userInput == 1:
-            runLyricsGenerator(lyricsModels)
-        elif userInput == 2:
-            songName = raw_input('What would you like to name your song? ')
-            runMusicGenerator(musicModels, 'wav/' + songName + '.wav')
-
-        print '\n',
-        userInput = getUserInput(teamName, lyricsSource, musicSource)
-
-    print '\nThank you for using the', teamName, 'music generator!'
+#return userInput
+#
+# def main():
+#     """
+#     Requires: nothing
+#     Modifies: nothing
+#     Effects:  this is your main function, which is done for you. It runs the
+#               entire generator program for both the reach and the core.
+#               It begins by loading the lyrics and music data, then asks the
+#               user to input a choice to generate either lyrics or music.
+#
+#               Note that for the core, only choice 1 (the lyrics generating
+#               choice) needs to be completed; if the user inputs 2, you
+#               can just have the runMusicGenerator function print "Under
+#               construction."
+#
+#               Also note that you can change the values of the first five
+#               variables based on your team's name, artist name, etc.
+#     """
+#     teamName = 'Team Name'
+#     lyricsSource = 'The Beatles'
+#     musicSource = 'Nintendo Gamecube'
+#     lyricsDirectory = 'the_beatles'
+#     musicDirectory = 'gamecube'
+#
+#     print 'Starting program and loading data...'
+#     lyricsModels = trainLyricsModels(lyricsDirectory)
+#     musicModels = trainMusicModels(musicDirectory)
+#     print 'Data successfully loaded\n'
+#
+#     userInput = getUserInput(teamName, lyricsSource, musicSource)
+#
+#     while userInput != 3:
+#         print '\n',
+#         if userInput == 1:
+#             runLyricsGenerator(lyricsModels)
+#         elif userInput == 2:
+#             songName = raw_input('What would you like to name your song? ')
+#             runMusicGenerator(musicModels, 'wav/' + songName + '.wav')
+#
+#         print '\n',
+#         userInput = getUserInput(teamName, lyricsSource, musicSource)
+#
+#     print '\nThank you for using the', teamName, 'music generator!'
 
 
 
 if __name__ == '__main__':
-    main()
+    #  main()
     # note that if you want to individually test functions from this file,
     # you can comment out main() and call those functions here. Just make
     # sure to call main() in your final submission of the project!
+    
+    a = UnigramModel()
+    runLyricsGenerator(a)
 
 
